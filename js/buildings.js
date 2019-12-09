4f4725a935b115ac148094b24d46781b585506a5
Jake Southward <jsouthward@me.com>
2019-12-09 15:20:22 +0000

    buildings.js layout
    Made the buildings file more organised,

--- js/buildings.js
+++ js/buildings.js
@@ -7,89 +7,16 @@ var geometryroad = new THREE.PlaneGeometry(12, 3.225, 2);
 var geometrypavement = new THREE.BoxGeometry(12, .5, 2);
 var geometrypavement2 = new THREE.BoxGeometry(15, .5, 2);
 
+
+//Building 1
 var textureb1 = new THREE.TextureLoader().load('https://i.imgur.com/mzNKmbI.jpg3');
 textureb1.wrapS = THREE.RepeatWrapping;
 textureb1.wrapT = THREE.RepeatWrapping;
 textureb1.repeat.set(1, 1);
 
-var textureb2 = new THREE.TextureLoader().load('https://i.imgur.com/Vyc51yj.jpg');
-textureb2.wrapS = THREE.RepeatWrapping;
-textureb2.wrapT = THREE.RepeatWrapping;
-textureb2.repeat.set(1, 1);
-
-//tesxtureb3 to add
-//textureb4 to add
-
-var textureb4 = new THREE.TextureLoader().load('https://i.imgur.com/b4gTFPE.jpg');
-textureb4.wrapS = THREE.RepeatWrapping;
-textureb4.wrapT = THREE.RepeatWrapping;
-textureb4.repeat.set(1, 1);
-
-var textureb5 = new THREE.TextureLoader().load('https://i.imgur.com/nmgvZ2y.jpg');
-textureb5.wrapS = THREE.RepeatWrapping;
-textureb5.wrapT = THREE.RepeatWrapping;
-textureb5.repeat.set(1.5, 1);
-
-var textureb6 = new THREE.TextureLoader().load('https://i.imgur.com/ai27Mai.jpg');
-textureb6.wrapS = THREE.RepeatWrapping;
-textureb6.wrapT = THREE.RepeatWrapping;
-textureb6.repeat.set(1.5, 1);
-
-var textureb7 = new THREE.TextureLoader().load('https://i.imgur.com/ZB0WK6M.jpg');
-textureb7.wrapS = THREE.RepeatWrapping;
-textureb7.wrapT = THREE.RepeatWrapping;
-textureb7.repeat.set(1, 8);
-
-var texturebfloor = new THREE.TextureLoader().load('https://i.imgur.com/lyBXGGd.jpg');
-texturebfloor.wrapS = THREE.RepeatWrapping;
-texturebfloor.wrapT = THREE.RepeatWrapping;
-texturebfloor.repeat.set(50, 50);
-
-var texturepavement = new THREE.TextureLoader().load('https://i.imgur.com/WDl05sW.jpg');
-texturepavement.wrapS = THREE.RepeatWrapping;
-texturepavement.wrapT = THREE.RepeatWrapping;
-texturepavement.repeat.set(6, 2.05);
-
-var textureroad = new THREE.TextureLoader().load('https://i.imgur.com/FkdVt4F.jpg');
-textureroad.wrapS = THREE.RepeatWrapping;
-textureroad.wrapT = THREE.RepeatWrapping;
-textureroad.repeat.set(1, 1);
-
-var materialfloor = new THREE.MeshPhongMaterial({ map: texturebfloor });
-
-var materialroad = new THREE.MeshPhongMaterial({
-    map: textureroad,
-    bumpMap: new THREE.TextureLoader().load('https://i.imgur.com/oaj63mP.jpg'),
-    shininess: 100
-});
-
-var materialpavement = new THREE.MeshPhongMaterial({
-    map: texturepavement,
-    shininess: 100
-});
-
-var materialLargeBuilding = new THREE.MeshPhongMaterial({ map: textureb5 });
-
-var materialLargeBuilding2 = new THREE.MeshPhongMaterial({ map: textureb6 });
-
-var materialTallBuilding = new THREE.MeshPhongMaterial({ map: textureb7 });
-
 var materialSmallBuilding = new THREE.MeshPhongMaterial({
     map: textureb1,
     bumpMap: new THREE.TextureLoader().load('https://i.imgur.com/IEvcznT.jpg')
-});
-
-var materialSmallBuilding2 = new THREE.MeshPhongMaterial({
-    map: textureb4,
-    bumpMap: new THREE.TextureLoader().load('https://i.imgur.com/zq1xrOV.jpg')
-});
-
-var materialMediumBuilding2 = new THREE.MeshPhongMaterial({ color: 0xff0000 });
-
-var materialMediumBuilding = new THREE.MeshPhongMaterial({
-    map: textureb2,
-    bumpMap: new THREE.TextureLoader().load('https://i.imgur.com/io4ThYx.jpg'),
-    shininess: 40
 });
 
 var b1 = new THREE.Mesh(geometrySmallBuilding, materialSmallBuilding);
@@ -99,6 +26,18 @@ b1.position.y = 1.2;
 b1.castShadow = true;
 scene.add(b1);
 
+//Building 2
+var textureb2 = new THREE.TextureLoader().load('https://i.imgur.com/Vyc51yj.jpg');
+textureb2.wrapS = THREE.RepeatWrapping;
+textureb2.wrapT = THREE.RepeatWrapping;
+textureb2.repeat.set(1, 1);
+
+var materialMediumBuilding = new THREE.MeshPhongMaterial({
+    map: textureb2,
+    bumpMap: new THREE.TextureLoader().load('https://i.imgur.com/io4ThYx.jpg'),
+    shininess: 40
+});
+
 var b2 = new THREE.Mesh(geometryMediumBuilding, materialMediumBuilding);
 b2.position.z = -4;
 b2.position.x = -.6;
@@ -107,12 +46,34 @@ b2.castShadow = true;
 b2.receiveShadow = true;
 scene.add(b2);
 
+//Building 3
+var textureb3 = new THREE.TextureLoader().load( 'https://i.imgur.com/vDgZ0Ck.jpg' );
+textureb3.wrapS = THREE.RepeatWrapping;
+textureb3.wrapT = THREE.RepeatWrapping;
+textureb3.repeat.set( 1, 1 );
+
+var materialMediumBuilding2 = new THREE.MeshPhongMaterial({ 
+  map: textureb3, 
+  bumpMap: new THREE.TextureLoader().load( 'https://i.imgur.com/6XpfGBK.jpg' )
+});
+
 var b3 = new THREE.Mesh(geometryMediumBuilding, materialMediumBuilding2);
 b3.position.z = 4;
 b3.position.x = 2.5;
 b3.position.y = 2.45;
 b3.castShadow = true;
 scene.add(b3);
+
+//Building 4
+var textureb4 = new THREE.TextureLoader().load('https://i.imgur.com/b4gTFPE.jpg');
+textureb4.wrapS = THREE.RepeatWrapping;
+textureb4.wrapT = THREE.RepeatWrapping;
+textureb4.repeat.set(1, 1);
+
+var materialSmallBuilding2 = new THREE.MeshPhongMaterial({
+    map: textureb4,
+    bumpMap: new THREE.TextureLoader().load('https://i.imgur.com/zq1xrOV.jpg')
+});
 
 var b4 = new THREE.Mesh(geometrySmallBuilding, materialSmallBuilding2);
 b4.position.z = 4.2;
@@ -121,6 +82,14 @@ b4.position.y = 1.2;
 b4.castShadow = true;
 scene.add(b4);
 
+//Building 5
+var textureb5 = new THREE.TextureLoader().load('https://i.imgur.com/nmgvZ2y.jpg');
+textureb5.wrapS = THREE.RepeatWrapping;
+textureb5.wrapT = THREE.RepeatWrapping;
+textureb5.repeat.set(1.5, 1);
+
+var materialLargeBuilding = new THREE.MeshPhongMaterial({ map: textureb5 });
+
 var b5 = new THREE.Mesh(geometryLargeBuilding, materialLargeBuilding);
 b5.position.x = 12;
 b5.position.y = 2.95;
@@ -128,12 +97,28 @@ b5.receiveShadow = true;
 b5.castShadow = true;
 scene.add(b5);
 
+//Building 6
+var textureb6 = new THREE.TextureLoader().load('https://i.imgur.com/ai27Mai.jpg');
+textureb6.wrapS = THREE.RepeatWrapping;
+textureb6.wrapT = THREE.RepeatWrapping;
+textureb6.repeat.set(1.5, 1);
+
+var materialLargeBuilding2 = new THREE.MeshPhongMaterial({ map: textureb6 });
+
 var b6 = new THREE.Mesh(geometryLargeBuilding, materialLargeBuilding2);
 b6.position.x = -10;
 b6.position.y = 2.95;
 b6.receiveShadow = true;
 b6.castShadow = true;
 scene.add(b6);
+
+//Building 7
+var textureb7 = new THREE.TextureLoader().load('https://i.imgur.com/ZB0WK6M.jpg');
+textureb7.wrapS = THREE.RepeatWrapping;
+textureb7.wrapT = THREE.RepeatWrapping;
+textureb7.repeat.set(1, 8);
+
+var materialTallBuilding = new THREE.MeshPhongMaterial({ map: textureb7 });
 
 var b7 = new THREE.Mesh(geometryTallBuilding, materialTallBuilding);
 b7.position.x = -12;
@@ -143,11 +128,32 @@ b7.receiveShadow = true;
 b7.castShadow = true;
 scene.add(b7);
 
+// floor, road, pavement
+// floor
+var texturebfloor = new THREE.TextureLoader().load('https://i.imgur.com/lyBXGGd.jpg');
+texturebfloor.wrapS = THREE.RepeatWrapping;
+texturebfloor.wrapT = THREE.RepeatWrapping;
+texturebfloor.repeat.set(50, 50);
+
+var materialfloor = new THREE.MeshPhongMaterial({ map: texturebfloor });
+
 var floor = new THREE.Mesh(geometryfloor, materialfloor);
 floor.rotateX(-Math.PI * 0.5);
 floor.position.y = -.55;
 floor.receiveShadow = true;
 scene.add(floor);
+
+// road
+var textureroad = new THREE.TextureLoader().load('https://i.imgur.com/FkdVt4F.jpg');
+textureroad.wrapS = THREE.RepeatWrapping;
+textureroad.wrapT = THREE.RepeatWrapping;
+textureroad.repeat.set(1, 1);
+
+var materialroad = new THREE.MeshPhongMaterial({
+    map: textureroad,
+    bumpMap: new THREE.TextureLoader().load('https://i.imgur.com/oaj63mP.jpg'),
+    shininess: 100
+});
 
 var road = new THREE.Mesh(geometryroad, materialroad);
 road.rotateX(-Math.PI * 0.5);
@@ -156,6 +162,17 @@ road.position.x = 1;
 road.receiveShadow = true;
 road.bumpScale = 0.1;
 scene.add(road);
+
+//pavement
+var texturepavement = new THREE.TextureLoader().load('https://i.imgur.com/WDl05sW.jpg');
+texturepavement.wrapS = THREE.RepeatWrapping;
+texturepavement.wrapT = THREE.RepeatWrapping;
+texturepavement.repeat.set(6, 2.05);
+
+var materialpavement = new THREE.MeshPhongMaterial({
+    map: texturepavement,
+    shininess: 100
+});
 
 var pavement = new THREE.Mesh(geometrypavement, materialpavement);
 pavement.receiveShadow = true;
