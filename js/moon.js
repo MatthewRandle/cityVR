0dba49c10907b86d905162cca65f8240e39af8c3
Jake Southward <jsouthward@me.com>
2019-12-12 19:49:13 +0000

    Added Moon

---
+++ js/moon.js
@@ -0,0 +1,8 @@
+var geometryMoon = new THREE.SphereGeometry(2, 50, 10);
+var materialMoon = new THREE.MeshBasicMaterial({color: 0xccc3af});
+
+var moon = new THREE.Mesh(geometryMoon, materialMoon);
+moon.position.z = 20;
+moon.position.x = 100;
+moon.position.y = 150;
+scene.add(moon);
